a65b14ce3a965879b2272b3522fd3990c10f200c
SpeedyM282 <a.rizakulov@student.inha.uz>
2022-12-28 15:44:50 +0500

    add main logic to App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+import { getName, getColor, getAge } from "./utils";
+import List from "./components/List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cats: [],
+      neighbourCats: [],
+    }
+    this.update = this.update.bind(this)
+  }
+
+  update(nextState) {
+    this.setState(nextState)
+  }
+
+  createCat = () => {
+    const newCat = {
+      name: getName(),
+      color: getColor(),
+      age: getAge(),
+      id: nanoid(),
+      hasCollar: Math.random() >= 0.5,
+      isHungry: false,
+      hungerTime: new Date().getTime() + 30000,
+    }
+
+    if (newCat.hasCollar) {
+      this.setState({ ...this.state, neighbourCats: [...this.state.neighbourCats, newCat] })
+    } else {
+      this.setState({ ...this.state, cats: [...this.state.cats, newCat] })
+    }
+  }
+
+  updateHungerTime = () => {
+    const currentTime = new Date().getTime();
+
+    const myCats = this.state.cats.map(cat => {
+      if (currentTime >= (cat.hungerTime)) {
+        cat.isHungry = true;
+      }
+      return cat;
+    }).filter(cat => {
+      const latestTimeToFeed = cat.hungerTime + 5000;
+      return currentTime < latestTimeToFeed
+    });
+
+    const neighboursCats = this.state.neighbourCats.map(cat => {
+      if (currentTime >= (cat.hungerTime)) {
+        cat.isHungry = true;
+      }
+      return cat;
+    }).filter(cat => {
+      const latestTimeToFeed = cat.hungerTime + 5000;
+      return currentTime < latestTimeToFeed
+    });
+
+    this.setState({
+      ...this.state,
+      cats: myCats,
+      neighbourCats: neighboursCats,
+    });
+  }
+
+  componentDidMount() {
+    const component = this;
+    setInterval(function () {
+      component.createCat();
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <div className="flex">
+        <List cats={this.state.cats} setter={this.update} hungerTimeUpdate={this.updateHungerTime} heading="First List" />
+        <List cats={this.state.neighbourCats} setter={this.update} hungerTimeUpdate={this.updateHungerTime} heading="Neighbor's List" />
+      </div>
+    );
+  }
 }
 
 export default App;
